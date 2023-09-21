1500518da217b4e0e04ed6cad43eabb9dfaa8566
what <whatagoal021@gmail.com>
2023-09-21 01:54:45 +0100

    3rd commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [], // Add any other external modules if needed
+    },
+  },
+});
+
